950154a4439f0691c46771faa0c2f46eae2d34e9
Steve Castle <steve@tigerhaunt.com>
2017-01-13 14:43:44 -0800

    frame player

---
+++ src/components/Grid/FramePlayer/FramePlayer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class FramePlayer extends Component {
+  static propTypes = {
+    frames: React.PropTypes.array,
+  };
+  render() {
+    return (
+      <div className="FramePlayer">
+      </div>
+    );
+  }
+}
+
+export default FramePlayer;
