2b43c944006c5db3852f2ac6f2956aa24e79caf6
Steve Castle <steve@tigerhaunt.com>
2017-01-13 10:39:49 -0800

    top level route components

---
+++ src/components/ArtBoard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class ArtBoard extends Component {
+  render() {
+    return (
+      <div className="ArtBoard">
+
+      </div>
+    );
+  }
+}
+
+export default ArtBoard;
